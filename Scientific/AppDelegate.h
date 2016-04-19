27f10a268d1ac9d12433c0aa8cfbd5da108bf1b5
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-04-18 20:14:36 -0400

    Scientific Calculator

---
+++ Scientific/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scientific
+//
+//  Created by Jorge Catalan on 4/18/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scientific/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scientific
+//
+//  Created by Jorge Catalan on 4/18/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
